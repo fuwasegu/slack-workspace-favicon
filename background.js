8f61097a701237d06b2695bd2c7b601d21202e54
horn553 <36446753+horn553@users.noreply.github.com>
2022-07-08 20:16:16 +0900

    :wastebasket: Remove: アイコンをクリックしてファビコンを更新する機能を削除

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-    if (tab.url.indexOf('https://app.slack.com') > -1) {
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            files: ['script.js']
-        });
-    }
-});
